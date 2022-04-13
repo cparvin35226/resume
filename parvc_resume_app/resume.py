489a5afc7a32e2f75667361affcbd9a4aa3e159f
Chris Parvin <cparvin@gmail.com>
2022-04-13 07:28:09 -0500

    real final commit

--- parvc_resume_app/resume.py
+++ parvc_resume_app/resume.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-def resume():
-    return ""
+
+@app.route('/')
+def home():
+   return render_template('resume-clean.html')
+if __name__ == '__main__':
+   app.run()
